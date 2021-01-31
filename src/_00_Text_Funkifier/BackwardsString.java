de792da0a741d1a2946468794366e0ba16b20133
Meiyi <hemeiyi0225@gmail.com>
2021-01-31 09:56:51 -0800

    Backward String

--- src/_00_Text_Funkifier/BackwardsString.java
+++ src/_00_Text_Funkifier/BackwardsString.java
@@ -8,8 +8,20 @@ public class BackwardsString extends SpecialString {
 
 	@Override
 	public String funkifyText(String s) {
+
+		char[] in = s.toCharArray();
+		int begin=0;
+	    int end=in.length-1;
+	    char temp;
+	    while(end>begin){
+	        temp = in[begin];
+	        in[begin]=in[end];
+	        in[end] = temp;
+	        end--;
+	        begin++;
+	    }
+	    return new String (in);
 		
-		return "";
 	}
 
 }
